eebf4e0a38a07465efa83461e24df19fb4168060
Alejandro Mery <amery@jpi.io>
2022-07-31 17:50:16 +0100

    expand: add module to expand shell variables before decoding config files
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ expand/file.go
@@ -0,0 +1,45 @@
+package expand
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+
+	"mvdan.cc/sh/v3/shell"
+)
+
+func ExpandString(s string, getEnv func(string) string) (string, error) {
+	if getEnv == nil {
+		getEnv = os.Getenv
+	}
+
+	return shell.Expand(s, getEnv)
+}
+
+func ExpandBytes(b []byte, getEnv func(string) string) (string, error) {
+	return ExpandString(string(b), getEnv)
+}
+
+func ExpandReader(f io.Reader, getEnv func(string) string) (string, error) {
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+
+	return ExpandString(string(b[:]), getEnv)
+}
+
+func ExpandFile(filename string, getEnv func(string) string) (string, error) {
+	var f io.Reader
+
+	if filename == "-" {
+		f = os.Stdin
+	} else if file, err := os.Open(filename); err != nil {
+		return "", err
+	} else {
+		defer file.Close()
+		f = file
+	}
+
+	return ExpandReader(f, getEnv)
+}
--- go.mod
+++ go.mod
@@ -11,4 +11,5 @@ require (
 	go.sancus.dev/core v0.16.0
 	gopkg.in/dealancer/validate.v2 v2.1.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	mvdan.cc/sh/v3 v3.5.1
 )
